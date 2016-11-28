8d5d19d0e91752eac0789301dd150aeaf3e18677
tungnguyentekcent <tung.nguyen@tekcent.com>
2016-11-28 21:46:32 +0700

    Add unit test for TransactionsApiController

---
+++ src/SampleFramework.Web.Tests/TransactionsApiControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SampleFramework.Web.Controllers;
+using SampleFramework.Web.Tests.Abstractions;
+
+namespace SampleFramework.Web.Tests
+{
+    [TestFixture]
+    public class TransactionsApiControllerTests : BaseUmbracoApiControllerTests
+    {
+        [Test]
+        public void GetTransactions_ShouldReturnListOfString()
+        {
+            // Arrange
+            var controller = new TransactionsApiController();
+
+            // Act
+            var result = controller.GetTransactions();
+
+            // Assert
+            Assert.IsNotNull(result);
+            CollectionAssert.AllItemsAreInstancesOfType(result, typeof(string));
+        }
+    }
+}
