b9efaf541ee966bef2abccc665694c2fdeae14c8
tungnguyentekcent <tung.nguyen@tekcent.com>
2016-11-28 22:34:22 +0700

    Add ContentSearchService with no implements

---
+++ src/SampleFramework.Services/ContentSearchService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Examine;
+using SampleFramework.Services.Interfaces;
+
+namespace SampleFramework.Services
+{
+    public class ContentSearchService : IContentSearchService
+    {
+        public ISearchResults SearchNodes(string alias)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
