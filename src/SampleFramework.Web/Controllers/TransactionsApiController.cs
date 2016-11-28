b102ed3cbcf7cbc45dcfb67be1628f2537743d57
tungnguyentekcent <tung.nguyen@tekcent.com>
2016-11-28 21:27:10 +0700

    Add BaseUmbracoApiController.cs and TransactionsApiController.cs

---
+++ src/SampleFramework.Web/Controllers/TransactionsApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleFramework.Web.Controllers
+{
+    public class TransactionsApiController : BaseUmbracoApiController
+    {
+        public IEnumerable<string> GetTransactions()
+        {
+            return new List<string>
+            {
+                "Transaction 1",
+                "Transaction 2",
+                "Transaction 3",
+                "Transaction 4",
+                "Transaction 5"
+            };
+        }
+    }
+}
